f40e78e4a09cddcfff9a70ecaab0a04af9797d2c
dllllllllllb <dllllllllllbpy27@gmail.com>
2020-06-18 15:54:21 +0100

    Made my test scene using whitebos and implemented my stuff to the main scene

---
+++ Module 5-6 Group Project/Assets/Scripts/MaciekScripts/ActivateSpawnerTemporary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSpawnerTemporary : MonoBehaviour
+{
+    private SpawnerV2 spawnerScript;
+    void Start()
+    {
+        spawnerScript = FindObjectOfType<SpawnerV2>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) { spawnerScript.SpawnerToggle(); }
+    }
+}
