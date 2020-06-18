17e62e19c5e8103ef1da823c9f90ea07fbd45ef6
dllllllllllb <dllllllllllbpy27@gmail.com>
2020-06-18 13:09:40 +0100

    Implemented my stuff in the main scene

---
+++ Module 5-6 Group Project/Assets/Scripts/MaciekScripts/ActivateSpawnerTemp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSpawnerTemp : MonoBehaviour
+{
+    private SpawnerV2 spawnerScript;
+
+    void Start()
+    {
+        spawnerScript = FindObjectOfType<SpawnerV2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) { spawnerScript.SpawnerToggle(); }
+    }
+}
