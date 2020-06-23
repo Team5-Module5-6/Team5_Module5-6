0cd78404e0114a0e268724fe29ef1d94e5a51a86
Adamjveasey <61789322+Adamjveasey@users.noreply.github.com>
2020-06-23 13:21:34 +0100

    added player stats script and set health to it

---
+++ Module 5-6 Group Project/Assets/Scripts/AdamScripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float Health = 100;
+
+    public TextMeshProUGUI healthUI;
+
+    private void Start()
+    {
+        healthUI.text = "Health: " + Mathf.Round(Health).ToString();
+    }
+
+    private void Update()
+    {
+        healthUI.text = "Health: " + Mathf.Round(Health).ToString();
+    }
+}
