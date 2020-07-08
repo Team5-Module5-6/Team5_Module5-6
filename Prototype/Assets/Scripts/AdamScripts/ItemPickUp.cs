fdbb4f21bed13756581fdae45ff991ca3271def5
Adamjveasey <61789322+Adamjveasey@users.noreply.github.com>
2020-07-08 21:18:15 +0100

    fixes

--- Prototype/Assets/Scripts/AdamScripts/ItemPickUp.cs
+++ Prototype/Assets/Scripts/AdamScripts/ItemPickUp.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemPickUp : MonoBehaviour
 {
     public float pickUpDistance;
     public Inventory inventory;
+    public GameObject popUp;
     private Item itemFound;
 
     void Start()
@@ -15,32 +17,35 @@ public class ItemPickUp : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            PickUp();
-        }
-    }
-
-    private void PickUp()
-    {
         RaycastHit raycastHit;
         if (Physics.Raycast(transform.position + transform.TransformDirection(Vector3.forward) * 0.5f, transform.TransformDirection(Vector3.forward), out raycastHit))
         {
-            if (raycastHit.transform.gameObject.CompareTag("Item") && raycastHit.distance < pickUpDistance && raycastHit.distance > 0.5)
+            if (raycastHit.transform.gameObject.CompareTag("Item") && raycastHit.distance < pickUpDistance)
             {
                 itemFound = raycastHit.transform.gameObject.GetComponent<Item>();
 
-                if (itemFound.generatorPart)
-                {
-                    inventory.FoundPart();
-                }
-                if (itemFound.labNote)
-                {
-                    inventory.FoundNote();
-                }
+                popUp.SetActive(true);
+                popUp.GetComponentInChildren<TextMeshProUGUI>().text = "Press E to pick up " + itemFound.itemName;
 
-                Destroy(raycastHit.transform.gameObject);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (itemFound.generatorPart)
+                    {
+                        inventory.FoundPart();
+                    }
+                    if (itemFound.labNote)
+                    {
+                        inventory.FoundNote();
+                    }
+
+                    Destroy(raycastHit.transform.gameObject);
+                }
+            }
+            else
+            {
+                popUp.SetActive(false);
             }
         }
+        
     }
 }
